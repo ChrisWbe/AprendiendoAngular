ca949e74dcaf3782c996cc27c23b4e6e52f9d8f4
Christian Quintero <chriswbe1993@gmail.com>
2020-05-25 14:22:40 -0500

    Pasar un valor desde fuera del componente, se utiliza decorador Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputDecoratorComponent } from './components/input-decorator/input-decorator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputDecoratorComponent
   ],
   imports: [
     BrowserModule
