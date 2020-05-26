0cc940ca9372348c06cdf02f820014d58644ea2c
Christian Quintero <chriswbe1993@gmail.com>
2020-05-26 12:28:39 -0500

    nfForOF permite crear alementos dinámicamente, con ayuda de las directivas

---
+++ src/app/components/ng-for-of/ng-for-of.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-ng-for-of',
+  templateUrl: './ng-for-of.component.html',
+  styleUrls: ['./ng-for-of.component.css']
+})
+export class NgForOfComponent implements OnInit {
+
+  public nameList:Array<string> = ['christian', 'wbeimar'];
+  constructor() { 
+
+  }
+
+  ngOnInit() {
+  }
+
+}
