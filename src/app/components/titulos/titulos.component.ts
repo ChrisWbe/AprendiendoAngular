5e61950f601ef497823af6fdf0f0a326fb265040
Christian Quintero <chriswbe1993@gmail.com>
2020-05-28 14:01:22 -0500

    dblclick, ngModelChange, inpt, focus, keydown, keyup, mouseenter, mouseleave

--- src/app/components/titulos/titulos.component.ts
+++ src/app/components/titulos/titulos.component.ts
@@ -20,7 +20,46 @@ export class TitulosComponent implements OnInit {
   }
 
   ingresar(){
-    console.log(`Correo: ${this.correo}, Contraseña: ${this.pass}`)
+    alert(`Correo: ${this.correo}, Contraseña: ${this.pass}`)
   }
 
+  escribirCorreo(){
+    console.log(this.correo);
+  }
+
+  escribirPass(){
+    console.log(this.pass);
+  }
+
+  escribir(event:any){
+    console.log(event.target.value);
+  }
+
+  colorearFondo(event:any){
+    event.srcElement.style.background="red";
+    console.log(event)
+  }
+
+  keyDown(event:any){
+    if(event.key == "Enter"){
+      alert("Enter");
+    }
+  }
+
+  keyUp(event:any){
+    if(event.key=="0"){
+      alert("0");
+    }
+  }
+
+  cambiarTamano(event:any){
+    event.srcElement.style.width = "500px";
+    event.srcElement.style.height="500px";
+
+  }
+
+  volverTamano(event:any){
+    event.srcElement.style.width = "100px";
+    event.srcElement.style.height="100px";
+  }
 }
