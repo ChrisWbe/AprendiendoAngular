f6a3fd880d8e5dc987161fd34f68d32de0531b5b
Christian Quintero <chriswbe1993@gmail.com>
2020-05-28 12:42:08 -0500

    Componentes, Interpolación, PropertyBinding, TwoWayBinding [(ngModel)]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TitulosComponent } from './components/titulos/titulos.component';
 
+import {FormsModule} from '@angular/forms'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitulosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
