9b87ea64a4e63c7d073bd04ac1039d6860876111
Christian Quintero <chriswbe1993@gmail.com>
2020-06-16 14:00:56 -0500

    Instalación de bootstrap, ngx-bootstrap para javascript, instalación de firebase para login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AccordionModule.forRoot(), //Para que funcione en toda la aplicación
+    AngularFireModule.initializeApp(environment.firebase)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCFTuejIAnH8SLrr0CedZDjQIIDhomlCe4",
+    authDomain: "mastergym-13085.firebaseapp.com",
+    databaseURL: "https://mastergym-13085.firebaseio.com",
+    projectId: "mastergym-13085",
+    storageBucket: "mastergym-13085.appspot.com",
+    messagingSenderId: "853867290824",
+    appId: "1:853867290824:web:2c80525d519af5a87985fd",
+    measurementId: "G-DGNTWEG1WK"
+  }
 };
 
 /*
