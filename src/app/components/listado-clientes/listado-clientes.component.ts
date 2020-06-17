bb2cd5e2459cf6b31b336cc8d52eb94a0d9644ea
Christian Quintero <chriswbe1993@gmail.com>
2020-06-17 13:09:36 -0500

    Obtener id del cliente, desde firebase

--- src/app/components/listado-clientes/listado-clientes.component.ts
+++ src/app/components/listado-clientes/listado-clientes.component.ts
@@ -12,8 +12,22 @@ export class ListadoClientesComponent implements OnInit {
   constructor(private db:AngularFirestore) { }
 
   ngOnInit() {
-    this.db.collection('clientes').valueChanges().subscribe((resultado)=>{
-      this.clientes=resultado;
+    // this.db.collection('clientes').valueChanges().subscribe((resultado)=>{
+    //   this.clientes=resultado;
+    //   console.log(resultado)
+    // })
+    this.clientes.length = 0;
+    this.db.collection('clientes').get().subscribe((resultado)=>{
+      resultado.docs.forEach((element)=>{
+        // console.log(element.id)
+        // console.log(element.data())
+        // console.log(element.ref)
+        let cliente = element.data();
+        cliente.id=element.id;
+        cliente.ref=element.ref;
+        this.clientes.push(cliente)
+      })
+      
     })
   }
 
